7955719ed79660d3c9f71bb1499f157e4c3f3a9e
wuhy <15048348071@163.com>
2020-06-10 11:46:54 +0800

    update 2020-06-10

--- ideaproject/springcloud/order-server/src/main/java/cn/wolfcode/springcloud/OrderServerApplication.java
+++ ideaproject/springcloud/order-server/src/main/java/cn/wolfcode/springcloud/OrderServerApplication.java
@@ -2,6 +2,7 @@ package cn.wolfcode.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,6 +13,7 @@ import javax.xml.ws.BindingType;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OrderServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServerApplication.class, args);
--- ideaproject/springcloud/order-server/src/main/java/cn/wolfcode/springcloud/controller/OrderController.java
+++ ideaproject/springcloud/order-server/src/main/java/cn/wolfcode/springcloud/controller/OrderController.java
@@ -2,16 +2,27 @@ package cn.wolfcode.springcloud.controller;
 
 import cn.wolfcode.springcloud.domain.Order;
 import cn.wolfcode.springcloud.service.IOrderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class OrderController {
     @Autowired
     private IOrderService orderService;
+
+    @HystrixCommand(fallbackMethod = "saverFail")
     @RequestMapping("/save")
     public Order save(Long userId, Long productId){
+        //模拟出错
+        //int i = 1 / 0;
         return orderService.save(userId,productId);
+    }
+
+    public Order saverFail(Long userId, Long productId){
+        System.out.println("返回兜底数据");
+        return new Order();
     }
 }
